d3c2bd6f9d774991186204036e48aa8fdb8b05eb
Flare1337 <dimonhek1234567@mail.ru>
2021-11-21 19:02:11 +0200

    SmallFix

--- src/com/company/File.java
+++ src/com/company/File.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class File {
     public static final String FILE_FOR_NUMBERS = "numbers.txt";
-    public static final String FILE_FOR_CONTACTS_ONLY = "text.txt";
+    public static final String FILE_FOR_CONTACTS_ONLY = "contacts.txt";
     public static final String FILE_FOR_TEXT = "text.txt";
 }
